66dee51709551ea26e4d4d5aaab453e931a7a084
Pontuagi <benepontuagi@gmail.com>
2024-01-16 22:34:58 +0000

    User Object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,9 @@
 BasicAuth Module
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -81,3 +83,34 @@ class BasicAuth(Auth):
         user_email, user_password = decoded_base64_authorization_header.split(
                 ':', 1)
         return user_email, user_password
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """
+        Get the User instance based on email and password.
+
+        Args:
+        - user_email (str): User's email.
+        - user_pwd (str): User's password.
+
+        Returns:
+        - TypeVar('User'): User instance or None if not found or
+        password doesn't match.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+
+        if not users:
+            return None
+
+        # Check if user_pwd is the password of the User instance found
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
