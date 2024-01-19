1be8bc0686db0fc696323fe1768f81540d8ef12f
Pontuagi <benepontuagi@gmail.com>
2024-01-19 13:43:25 +0300

    Creating a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,7 +18,7 @@ class SessionAuth(Auth):
     Returns:
         str: Session ID
     """
-    user_session_id = {}
+    user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """
@@ -28,6 +28,6 @@ class SessionAuth(Auth):
             return None
 
         session_id = str(uuid.uuid4())
-        self.user_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
 
         return session_id
